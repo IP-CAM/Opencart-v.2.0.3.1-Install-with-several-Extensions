758426d1cb7ec7fcd40102a74fa24ed08affa740
Anders Jacobsson <anders.jacobsson@gmail.com>
2015-06-16 21:13:41 +0200

    Updated configuration files

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://192.168.4.159/opencart2/admin/');
+define('HTTP_CATALOG', 'http://192.168.4.159/opencart2/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://192.168.4.159/opencart2/admin/');
+define('HTTPS_CATALOG', 'http://192.168.4.159/opencart2/');
+
+// DIR
+define('DIR_APPLICATION', '/home/adminuser/sf/sf_bilka/opencart2/admin/');
+define('DIR_SYSTEM', '/home/adminuser/sf/sf_bilka/opencart2/system/');
+define('DIR_LANGUAGE', '/home/adminuser/sf/sf_bilka/opencart2/admin/language/');
+define('DIR_TEMPLATE', '/home/adminuser/sf/sf_bilka/opencart2/admin/view/template/');
+define('DIR_CONFIG', '/home/adminuser/sf/sf_bilka/opencart2/system/config/');
+define('DIR_IMAGE', '/home/adminuser/sf/sf_bilka/opencart2/image/');
+define('DIR_CACHE', '/home/adminuser/sf/sf_bilka/opencart2/system/cache/');
+define('DIR_DOWNLOAD', '/home/adminuser/sf/sf_bilka/opencart2/system/download/');
+define('DIR_UPLOAD', '/home/adminuser/sf/sf_bilka/opencart2/system/upload/');
+define('DIR_LOGS', '/home/adminuser/sf/sf_bilka/opencart2/system/logs/');
+define('DIR_MODIFICATION', '/home/adminuser/sf/sf_bilka/opencart2/system/modification/');
+define('DIR_CATALOG', '/home/adminuser/sf/sf_bilka/opencart2/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'opencart2');
+define('DB_PASSWORD', 'opencart2');
+define('DB_DATABASE', 'opencart2');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://192.168.4.159/opencart2/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://192.168.4.159/opencart2/');
+
+// DIR
+define('DIR_APPLICATION', '/home/adminuser/sf/sf_bilka/opencart2/catalog/');
+define('DIR_SYSTEM', '/home/adminuser/sf/sf_bilka/opencart2/system/');
+define('DIR_LANGUAGE', '/home/adminuser/sf/sf_bilka/opencart2/catalog/language/');
+define('DIR_TEMPLATE', '/home/adminuser/sf/sf_bilka/opencart2/catalog/view/theme/');
+define('DIR_CONFIG', '/home/adminuser/sf/sf_bilka/opencart2/system/config/');
+define('DIR_IMAGE', '/home/adminuser/sf/sf_bilka/opencart2/image/');
+define('DIR_CACHE', '/home/adminuser/sf/sf_bilka/opencart2/system/cache/');
+define('DIR_DOWNLOAD', '/home/adminuser/sf/sf_bilka/opencart2/system/download/');
+define('DIR_UPLOAD', '/home/adminuser/sf/sf_bilka/opencart2/system/upload/');
+define('DIR_MODIFICATION', '/home/adminuser/sf/sf_bilka/opencart2/system/modification/');
+define('DIR_LOGS', '/home/adminuser/sf/sf_bilka/opencart2/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'opencart2');
+define('DB_PASSWORD', 'opencart2');
+define('DB_DATABASE', 'opencart2');
+define('DB_PREFIX', 'oc_');
