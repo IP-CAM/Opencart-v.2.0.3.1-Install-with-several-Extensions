17d8c22b84d07fb40153bf37e945e54e70ca2819
Anders Jacobsson <anders.jacobsson@gmail.com>
2015-06-22 22:22:20 +0200

    Added and tweaked Swedish language pack

---
+++ admin/language/swedish/extension/total.php
@@ -0,0 +1,16 @@
+<?php
+// Heading
+$_['heading_title']     = 'Ordersumma';
+
+// Text
+$_['text_success']      = 'Klart: Dina ändringar i ordersumma är utförda!';
+$_['text_list']         = 'Ordersumma';
+
+// Column
+$_['column_name']       = 'Namn';
+$_['column_status']     = 'Status';
+$_['column_sort_order'] = 'Sorteringsordning';
+$_['column_action']     = 'Åtgärd';
+
+// Error
+$_['error_permission']  = 'Varning: Du har inte behörighet att i ordersumma!';
