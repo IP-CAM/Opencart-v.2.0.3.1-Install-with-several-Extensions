ce64c6dd03afd2206c83d9ef56e1e0d916c4da52
Anders Jacobsson <anders.jacobsson@gmail.com>
2015-08-19 22:21:04 +0200

    Fixed wording

--- catalog/language/swedish/common/maintenance.php
+++ catalog/language/swedish/common/maintenance.php
@@ -4,4 +4,4 @@ $_['heading_title']    = 'Underhåll';
 
 // Text
 $_['text_maintenance'] = 'Underhåll';
-$_['text_message']     = '<h1 style="text-align:center;">Vi är för närvarande underhåll av sidan. <br/>Vi försöker göra allt klart så fort som möjligt. Kom gärna tillbaka snart igen.</h1>';
+$_['text_message']     = '<h1 style="text-align:center;">Vi gör för närvarande underhåll av sidan. <br/>Vi försöker göra allt klart så fort som möjligt. Kom gärna tillbaka snart igen.</h1>';
