45894a7d348613eca7779ca0e6e4e53bfbcc9d70
Anders Jacobsson <anders.jacobsson@gmail.com>
2015-08-25 22:24:21 +0200

    Added missing translation

---
+++ catalog/language/swedish/module/openstock.php
@@ -0,0 +1,9 @@
+<?php
+$_['text_out_of_stock']     = 'Ej i lager!';
+$_['text_in_stock']         = '%s artikel/artiklar i lager';
+$_['text_in_stock_avail']   = 'I lager';
+$_['combination_not_avail'] = 'Valt alternativ ej tillgängligt';
+$_['text_id_missing']       = 'Ogiltlig förfrågan';
+$_['text_checking_options'] = 'Kollar status';
+$_['text_discount']         = ' eller mer ';
+$_['text_nodiscount']       = 'Ingen kvantitetsrabatt tillgänglig';
