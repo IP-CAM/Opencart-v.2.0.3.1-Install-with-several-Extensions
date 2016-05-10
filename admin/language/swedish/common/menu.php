497cc56e0ba62660295337eb52de9477ec896bfa
Anders Jacobsson <anders.jacobsson@gmail.com>
2016-05-10 21:16:04 +0200

    Workaround to get SEO Power Pack to work with only Swedish installed

--- admin/language/swedish/common/menu.php
+++ admin/language/swedish/common/menu.php
@@ -1,6 +1,7 @@
 <?php
 // Text
 $_['text_affiliate']                   = 'Affiliates';
+$_['text_seo_power_pack']                         = 'SEO Power Pack';
 $_['text_api']                         = 'API';
 $_['text_attribute']                   = 'Attribut';
 $_['text_attribute_group']             = 'Attributgrupper';
