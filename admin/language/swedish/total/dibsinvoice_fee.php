64c3d9021e21b8462717c08fb60b095a1d207ccb
Anders Jacobsson <anders.jacobsson@gmail.com>
2016-06-18 08:52:29 +0200

    Removed DIBS Payment Window in favor of DIBS FlexWin

--- admin/language/swedish/total/dibsinvoice_fee.php
+++
@@ -1,9 +0,0 @@
-<?php 
-// Heading
-$_['heading_title']    = 'DIBS invoice fee';
-
-$_['entry_status']     = 'Ststus';
-
-$_['entry_sort_order']  = 'Sort order';
-
-$_['text_edit']         = 'Edit Dibsinvoice fee';
