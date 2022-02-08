b65e6b6ae39897cb57f43dd75aa698e0332c1427
Anders Jacobsson <anders.jacobsson@gmail.com>
2022-02-08 21:36:20 +0100

    Added stuff for Bambora online checkout

---
+++ catalog/language/swedish/payment/bambora_online_checkout.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Copyright (c) 2017. All rights reserved Bambora Online.
+ *
+ * This program is free software. You are allowed to use the software but NOT allowed to modify the software.
+ * It is also not legal to do any changes to the software and distribute it in your own name / brand.
+ *
+ * All use of the payment modules happens at your own risk. We offer a free test account that you can use to test the module.
+ *
+ * @author    Bambora Online
+ * @copyright Bambora Online (https://bambora.com)
+ * @license   Bambora Online
+ *
+ */
+
+// Text
+$_['text_title'] = "Bambora Online Checkout";
+$_['text_loading'] = "Laddar...";
+$_['text_payment'] = "Genom att klicka på <b>Betala nu</b> så kommer du bli vidarebefodrad till betalningsfönstret för Bambora Online Checkout.";
+$_['text_shipping_id'] = "frakt";
+$_['text_coupon_id'] = "rabattkod";
+$_['text_voucher_id'] = "presentkortskod";
+$_['button_confirm'] = "Betala nu";
+$_['error_payment_window'] = "Ett fel uppstod när betalningsfönstret skulle öppnas. Orsak:";
+
+$_['payment_process']   = "Betalning godkänd med beloppet:";
+$_['payment_with_transactionid']   = "Transaktionsnummer:";
+$_['payment_card']   = "Betalningsmetod:";
+
+$_['pcs'] = "stk.";
+$_['surcharge_fee'] = "Transaktionsavgift:";
+$_['accept_order_text'] = "Avvaktar betalningsbekräftelse från Bambora";
+
+//Format
+$_['date_format'] = 'Y/m/d';
+$_['currency_decimal_point'] = ',';
+$_['currency_thousand_separator'] = '.';
