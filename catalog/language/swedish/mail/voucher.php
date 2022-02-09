872c7954d00e9e42fb5fe1d94f447dfe05efb0eb
Anders Jacobsson <anders.jacobsson@gmail.com>
2022-02-09 08:13:25 +0100

    Updated mail footer texts now that they are sent from a no-reply address

--- catalog/language/swedish/mail/voucher.php
+++ catalog/language/swedish/mail/voucher.php
@@ -5,4 +5,4 @@ $_['text_greeting'] = 'Grattis! Du har fått ett presentkort värt %s';
 $_['text_from']     = 'Detta presentkort har skickats till dig från %s';
 $_['text_message']  = 'Med följande meddelande';
 $_['text_redeem']   = 'För att använda detta presentkort, skriv ner koden som är<b>%s</b> Klicka sedan på länken nedan och välj den produkt du vill använda presentkortet för. Ange presentkortskoden i varukorgen innan du klickar dig vidare till kassan.';
-$_['text_footer']   = 'Du kan svara på detta e-post om du har några frågor.';
+$_['text_footer']   = 'Vid frågor, vänligen kontakta oss på kenth@bilka.se.';
